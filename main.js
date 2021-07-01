3dab226faf59a349710170ff8493cacb40439dda
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-01 19:21:48 +0700

    4.2-4.3

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const app = Vue.createApp({
     data(){
         return{
-            product: 'Books',
+            product: 'Shoes',
             discription :'This book is best seller in the world',
             image: './assets/images/socks_green.jpg',
-            url_link: 'https://www.camt.cmu.ac.th'
+            url_link: 'https://www.camt.cmu.ac.th',
+            inStock : false
         }
     }
 })
