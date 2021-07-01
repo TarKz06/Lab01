fcffd32e170d1d962b06599cf21c7104ca9389b4
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-01 18:49:37 +0700

    3.2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Books',
-            discription :'This book is best seller in the world'
+            discription :'This book is best seller in the world',
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
