3cafb0dedfd06f02f16deaa82d07547363684982
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-01 19:48:38 +0700

    5.3

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const app = Vue.createApp({
             inStock : false,
             inventory : 100,
             details: ['50% cotton', '30% woll' , '20% polyester'],
+            variants: [
+                { id : 2234 , color : 'green'},
+                { id : 2235 , color : 'blue'}
+            ],
             onSale : true
         }
     }
